0841c6eaa4844d9b56e8025127c085312bac8acd
podchasova11 <pod4asova20@gmail.com>
2026-02-06 20:11:39 +0300

    added new file

--- component_01.py
+++ component_01.py
@@ -9,13 +9,19 @@ name = "Mila"
 age = "10"
 message = "Меня зовут "  + name  +  " и мне " + age + " лет."
 print(message)
+message_f = f"Меня зовут {name} и мне {age} лет."
+print(message_f)
+text_to_escape = "Это \"пример\" строки с символами: ', и обратной косой чертой \\."
+print(text_to_escape)
+# text_to_escape_1 = r'Это "пример" строки с символами: '', и обратной косой чертой \.'
+# print(text_to_escape_1)
 
 text = "Python - лучший язык для автоматизации!"
 contains = "Python" in text  # содержит ли строка text слово "Python"
 print(contains)
 text_modified = text.replace(" ", "*")# В строке text, замените все пробелы на *
 print(text_modified)
-print(text[-1] == "!")  # Проверьте, заканчивается ли строка text на восклицательный знак.
+print(text[-1] == "!")  # Проверьте, заканчивается ли строка text на восклицательный знак
 print(text.upper())
-print(text.lower()) # Преобразуйте всю строку в нижний или верхний регистр
+print(text.lower())  # Преобразуйте всю строку в нижний или верхний регистр
 
