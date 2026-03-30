4a0e0d319d1fb1f18410c1b9373ad0b1b9de1416
podchasova11 <pod4asova20@gmail.com>
2026-03-30 13:13:23 +0300

    added new in  components_06

--- component_06.py
+++ component_06.py
@@ -7,3 +7,16 @@ driver = webdriver.Chrome()
 
 driver.close() # Закрытие одной вкладки, если их несколько
 driver.quit() # Закрывает все окна браузера, в целом закрывает всю сессию
+
+class Person:
+    age = 23
+
+person = Person()
+print(getattr(person, 'age'))  # Выведет 23
+setattr(person, 'age', 30)
+print(person.age)  # Выведет 30
+
+double = lambda x: x * 2
+print(double(5))  # Выведет 10
+
+enumerate(iterable, start=0)
