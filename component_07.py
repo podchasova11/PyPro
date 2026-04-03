65d1ef2264500ca56d875fa859b835114d48f906
podchasova11 <pod4asova20@gmail.com>
2026-04-03 09:06:04 +0300

    added new options

--- component_07.py
+++ component_07.py
@@ -59,42 +59,77 @@
 # print(button_submit.is_enabled())
 
 
-
-
+# from selenium import webdriver
+# import time
+#
+# driver = webdriver.Chrome()
+#
+# driver.get("https://the-internet.herokuapp.com/login")
+# time.sleep(1)
+#
+# username_field = driver.find_element("xpath", "//input[@id='username']")
+# time.sleep(1)
+# assert username_field.get_attribute("value") == ""
+# username_field.send_keys("tomsmith")
+# username_field_value = username_field.get_attribute("value")
+# assert "tomsmith" in username_field_value
+#
+# password_field = driver.find_element("xpath", "//input[@id='password']")
+# # Очищаем поле
+# password_field.clear()
+# # Проверяем, что в поле пусто
+# assert password_field.get_attribute("value") == ""
+# # Вводим новое значение в поле
+# password_field.send_keys("SuperSecretPassword!")
+# # Создаем переменную email_field_value и присваиваем ей значение
+# password_field_value = password_field.get_attribute("value")
+# # Проверяем что в поле password содержится введенный password
+# assert "SuperSecretPassword!" in password_field_value
+#
+# button_submit = driver.find_element("xpath", "//button[@class='radius']")
+# time.sleep(1)
+# assert button_submit.is_enabled() == True, "Button [Submit] is disabled"
+# print(button_submit.is_enabled())
+#
+# button_submit.click()
 
 from selenium import webdriver
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import InvalidSwitchToTargetException
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 5)
 
-driver.get("https://the-internet.herokuapp.com/login")
-time.sleep(1)
+try:
+    driver.get("https://the-internet.herokuapp.com/login")
 
-username_field = driver.find_element("xpath", "//input[@id='username']")
-time.sleep(1)
-assert username_field.get_attribute("value") == ""
-username_field.send_keys("tomsmith")
-username_field_value = username_field.get_attribute("value")
-assert "tomsmith" in username_field_value
+    # Ввод учётных данных
+    username = wait.until(EC.presence_of_element_located((By.ID, "username")))
+    username.send_keys("tomsmith")
 
-password_field = driver.find_element("xpath", "//input[@id='password']")
-# Очищаем поле
-password_field.clear()
-# Проверяем, что в поле пусто
-assert password_field.get_attribute("value") == ""
-# Вводим новое значение в поле
-password_field.send_keys("SuperSecretPassword!")
-# Создаем переменную email_field_value и присваиваем ей значение
-password_field_value = password_field.get_attribute("value")
-# Проверяем что в поле password содержится введенный password
-assert "SuperSecretPassword!" in password_field_value
+    password = wait.until(EC.presence_of_element_located((By.ID, "password")))
+    password.clear()
+    password.send_keys("SuperSecretPassword!")
 
-button_submit = driver.find_element("xpath", "//button[@class='radius']")
-time.sleep(1)
-assert button_submit.is_enabled() == True, "Button [Submit] is disabled"
-print(button_submit.is_enabled())
+    # Клик по кнопке Login
+    login_btn = wait.until(EC.element_to_be_clickable(("xpath", "//button[@class='radius']")))
+    try:
+        login_btn.click()
+    except (InvalidSwitchToTargetException):
+        print("Thrown when frame or window target to be switched doesn't exist")
 
-button_submit.click()
+    # Валидация успешного входа
+    wait.until(EC.url_to_be("https://the-internet.herokuapp.com/secure"))
+    assert "secure" in driver.current_url, "Не перешли на /secure"
+
+    print("✅ Успешный вход подтверждён")
+
+finally:
+    driver.quit()
+
+
 
 
 
