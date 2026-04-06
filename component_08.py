3c3ebbdfec51b7b4af778ebf6db9bf520df79255
podchasova11 <pod4asova20@gmail.com>
2026-04-06 17:17:30 +0300

    added new options

--- component_08.py
+++ component_08.py
@@ -293,10 +293,14 @@ import os
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 
 # Locator (improved: use By.XPATH instead of raw tuple)
-VISIBLE_AFTER_BUTTON = (By.XPATH, "//div[@id='delayedText']")
+VISIBLE_AFTER_BUTTON = ("xpath", "//div[@id='delayedText']")
+DISAPPEARS_TEXT = ("xpath", "//div[@id='deletesuccess']")
+CLICKABLE_BUTTON = ("xpath", "//input[@id='timerButton']")
+TRY_IT_BUTTON = ("xpath", "//button[text()='Try it']")
+MY_BUTTON = ("xpath", "//button[@id='myBtn']")
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")  # 'add_argument'
@@ -306,12 +310,31 @@ wait = WebDriverWait(driver, 30, poll_frequency=1)
 
 try:
     driver.get("https://omayo.blogspot.com/")
+
+    # Wait to become *invisiblity*
+    wait.until(EC.invisibility_of_element_located(DISAPPEARS_TEXT))
+    print("✅ Text invisiblity successfully.")
+
     # Wait for the button to become *visible* (not just present)
     wait.until(EC.visibility_of_element_located(VISIBLE_AFTER_BUTTON))
 
-    # Click it
-    button = driver.find_element(*VISIBLE_AFTER_BUTTON)
-    button.click()
-    print("✅ Button clicked successfully.")
+    print("✅ Button [VISIBLE_AFTER_BUTTON] visible successfully.")
+
+    # # Wait button to become *clickable*
+    wait.until(EC.element_to_be_clickable(CLICKABLE_BUTTON))  # Ждем пока кнопка станет кликабельной (видимой + enabled)
+
+    print("✅ Button [CLICKABLE_BUTTON] to become *clickable* successfully.")
+
+    # # Wait button  [TRY_IT_BUTTON] to become *clickable*
+    wait.until(EC.element_to_be_clickable(TRY_IT_BUTTON)).click()  # Ждем пока кнопка станет кликабельной (видимой + enabled)
+
+    print("✅ Button [TRY_IT_BUTTON] to become *clickable* successfully.")
+
+    # Ждем, пока атрибут 'disabled' элемента станет равным "true"
+    wait.until(lambda driver: driver.find_element(*MY_BUTTON).get_attribute("disabled") == "true")
+
+    print("✅ Button [MY_BUTTON] to become disabled")
+
+
 finally:
     driver.quit()
